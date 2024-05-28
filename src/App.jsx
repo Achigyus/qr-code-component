6ba7a356a6068f6b8bc341637d73c57c44a2cca6
Achi Gyuse Achi <70823478+Achigyus@users.noreply.github.com>
2024-05-28 09:40:03 +0100

    update img tag alt attribute

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className="container">
       <div className="qr_code_card">
         <div className="qr_code_img_container">
-          <img src={qrCode} alt="qr_code_to_visit_frontendmentor" className="qr_code" />
+          <img src={qrCode} alt="QR code to frontendmentor.io" className="qr_code" />
         </div>
         <div className="qr_code_card_text">
           <h1 className="qr_code_h1 outfit-700">Improve your front-end skills by building projects</h1>
